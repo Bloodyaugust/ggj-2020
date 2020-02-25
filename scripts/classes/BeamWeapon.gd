e7e1ad69315f435746fa7872b61d29815a2001b5
Greyson Richey <greysonrichey@gmail.com>
2020-02-25 02:14:03 -0700

    Finish all non-damage beam weapon implementation
    - Add laser node and enemy
    - Fix player scene offset

--- scripts/classes/BeamWeapon.gd
+++ scripts/classes/BeamWeapon.gd
@@ -29,13 +29,13 @@ func fire():
     _beam_weapon_state = BEAM_WEAPON_STATES.FIRING
 
 func _on_node_connected():
-  team = parent.team
+  _set_team(parent.team)
   root_ship_node = parent.root_ship_node
   root_ship_node.connect("weapon_fire", self, "fire")
   root_ship_node.connect("weapon_set_look", self, "_on_weapon_set_look")
 
 func _on_node_disconnected():
-  team = 0
+  _set_team(0)
   root_ship_node.disconnect("weapon_fire", self, "fire")
   root_ship_node.disconnect("weapon_set_look", self, "_on_weapon_set_look")
   root_ship_node = null
@@ -46,8 +46,16 @@ func _on_weapon_set_look(at: Vector2):
 func _physics_process(delta):
   if _beam_weapon_state == BEAM_WEAPON_STATES.FIRING:
     raycast.enabled = true
+    line.visible = true
+
+    if raycast.is_colliding():
+      line.points[1] = line.to_local(raycast.get_collision_point())
+    else:
+      line.points[1] = Vector2(beam_range, 0)
+
   else:
     raycast.enabled = false
+    line.visible = false
 
 func _process(delta):
   if _beam_weapon_state == BEAM_WEAPON_STATES.FIRING:
@@ -55,8 +63,6 @@ func _process(delta):
     
     if _current_heat >= heat_capacity:
       _beam_weapon_state = BEAM_WEAPON_STATES.COOLDOWN
-    else:
-      _beam_weapon_state = BEAM_WEAPON_STATES.IDLE
 
   if _beam_weapon_state == BEAM_WEAPON_STATES.COOLDOWN && _current_heat <= 0:
     _beam_weapon_state = BEAM_WEAPON_STATES.IDLE
@@ -66,5 +72,14 @@ func _process(delta):
 func _ready():
   _beam_weapon_state = BEAM_WEAPON_STATES.IDLE
 
+  raycast.cast_to = Vector2(beam_range, 0)
+
   parent.connect("node_connected", self, "_on_node_connected")
   parent.connect("node_disconnected", self, "_on_node_disconnected")
+
+func _set_team(new_team: int):
+  raycast.set_collision_mask_bit(team, true)
+
+  team = new_team
+
+  raycast.set_collision_mask_bit(team, false)
